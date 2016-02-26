fba3706a009bcff5f25825863ab1fa43d385e56a
Kuan Butts <kuanbutts@yahoo.com>
2016-02-26 16:39:02 -0700

    basic version

--- app.js
+++ app.js
@@ -3,19 +3,31 @@ var express = require("express");
 var app = express();
 
 // dependencies
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+// database
+var pg = require("pg");
+var creds = require("./creds.js")
+var conString = "postgres://" + creds.user + ":" + creds.password + "@" + creds.host + ":5432/" + creds.database;
+var client = new pg.Client(conString);
+client.connect();
+
+
 app.get("/", function (req, res) {
-	res.send("index");
+	res.render("index");
+});
+
+app.post("/", function (req, res) {
+	res.render("index");
 });
 
 
-var port = 4040;
+var port = 8080;
 app.listen(port, function () { console.log("Listening on port", port); });
